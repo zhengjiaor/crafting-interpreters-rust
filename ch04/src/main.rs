799bef5c1a6b2f22cdf8474730e13d84206af923
zhengjiaor <mhl1983+2@gmail.com>
2024-07-26 17:35:26 +0800

    read input

--- ch04/src/main.rs
+++ ch04/src/main.rs
@@ -1,3 +1,35 @@
+use std::{env, fs, io::{self, Write}, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() > 1 {
+        println!("Usage: jlox [script]");
+        process::exit(64);
+    } else if args.len() == 1 {
+        run_file(&args[0]);
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(path: &str) {
+    let source = fs::read_to_string(path).expect(format!("Failed to read file: {}", path).as_str());
+    run(&source);
+}
+
+fn run_prompt() {
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        if line.is_empty() {
+            break;
+        }
+        run(&line);
+    }
+}
+
+fn run(source: &str) {
+    println!("{}", source);
 }
